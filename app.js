0c96cb070fe62c47bf4af5e285427113b26663a0
Oshamaru <callMeSam808@gmail.com>
2022-06-08 11:20:41 -1000

    Install mongoose and convert syntax to mongoose.

--- app.js
+++ app.js
@@ -1,49 +1,64 @@
-const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'fruitsDB';
+main().catch(err => console.log(err));
 
 async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('fruits');
+  await mongoose.connect('mongodb://localhost:27017/fruitsDB');
 
-  // the following code examples can be pasted here...
-  const insertResult = await collection.insertMany([
-    { 
-      name: "Apple",
-      score: 8,
-      review: "Great fruit" 
-    }, 
-    { 
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour" 
-    }, 
-    { 
-      name: "Banana",
-      score: 9,
-      review: "Great stuff" 
+  const fruitSchema = new mongoose.Schema({
+    name: String,
+    score: Number,
+    review: String
+  });
+
+  const Fruit = mongoose.model('Fruit', fruitSchema);
+
+  const apple = new Fruit({ 
+    name: 'Apple',
+    score: 8,
+    review: "Great fruit"
+  });
+
+  // await apple.save();
+
+  const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+  });
+
+  const Person = mongoose.model('Person', personSchema);
+
+  const person = new Person({ 
+    name: 'John',
+    age: 28,
+  });
+
+  // await person.save();
+
+  const kiwi = new Fruit({
+    name: 'Kiwi',
+    score: 10,
+    review: "The best fruit!"
+  });
+
+  const orange = new Fruit({
+    name: 'Orange',
+    score: 4,
+    review: "Too sour for me"
+  });
+
+  const banana = new Fruit({
+    name: 'Banana',
+    score: 3,
+    review: "Weird texture"
+  });
+
+  Fruit.insertMany([kiwi, orange, banana], (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully saved all the fruits to fruitsDB");
     }
-  ]);
-  console.log('Inserted documents =>', insertResult);
-
-  const findResult = await collection.find({}).toArray();
-  console.log('Found documents =>', findResult);
-
-  return 'done.';
+  });
 }
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
 
